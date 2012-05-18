83d48efd377852894fea6f7ca7ce77e0cbd267ad
unknown <Luca@Luca-PC.(none)>
2012-05-18 17:59:49 +0200

    primo commit

---
+++ Prove_WLAN/Prove_WLAN/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : file di inclusione per file di inclusione di sistema standard
+// o file di inclusione specifici del progetto utilizzati di frequente, ma
+// modificati raramente
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: fare riferimento qui alle intestazioni aggiuntive richieste dal programma
+#include <windows.h>
+#include <wlanapi.h>
+#include <objbase.h>
+#include <wtypes.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Need to link with Wlanapi.lib and Ole32.lib
+#pragma comment(lib, "wlanapi.lib")
+#pragma comment(lib, "ole32.lib")
