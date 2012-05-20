1b8c8605055804a636936a25bcd400a417927dc5
lepitrust@gmail.com <admin@max-mionotebook.(none)>
2012-05-20 21:47:01 +0200

    added namespace

--- Prove_WLAN/Prove_WLAN/MAC_test.cpp
+++ Prove_WLAN/Prove_WLAN/MAC_test.cpp
@@ -1,6 +1,6 @@
 #include "StdAfx.h"
 #include "MAC_test.h"
-
+using namespace std;
 
 MAC_test::MAC_test(void)
 {
@@ -11,19 +11,20 @@ MAC_test::~MAC_test(void)
 {
 }
 
-void MAC_test::InsertElement(std::string MacAddress, int iRSSI)
+void MAC_test::InsertElement(string MacAddress, int iRSSI)
 {
-	lMacAddress.insert(std::make_pair(MacAddress,iRSSI));
+	lMacAddress.insert(make_pair(MacAddress,iRSSI));
 }
-float MAC_test::ErrorValue(std::map<std::string, int> lMacAddress_testing)
+float MAC_test::ErrorValue(map<string, int> lMacAddress_testing)
 {
 	/*Utilizza una funzione del average mean square (media quadratica)*/ 
+	using namespace std;
 	float error_value=0;
 	int nValue=0;
-	for(std::map<std::string, int>::iterator ii=lMacAddress_testing.begin(); ii!=lMacAddress_testing.end(); ++ii)
+	for(map<string, int>::iterator ii=lMacAddress_testing.begin(); ii!=lMacAddress_testing.end(); ++ii)
 	{
 		float diff=(lMacAddress[(*ii).first])-(*ii).second;
-		std::cout << "[MAC_scan:Memory-iRSSI=diff] " << (*ii).first << ": (" << (lMacAddress[(*ii).first]) << ")-(" << (*ii).second << ")=(" << diff << ")" << std::endl;
+		cout << "[MAC_scan:Memory-iRSSI=diff] " << (*ii).first << ": (" << (lMacAddress[(*ii).first]) << ")-(" << (*ii).second << ")=(" << diff << ")" << std::endl;
 		if(abs(diff)!=abs((*ii).second))
 		{
 			//Esiste nella mappa così dovrei avere solo quelli comuni
@@ -31,7 +32,7 @@ float MAC_test::ErrorValue(std::map<std::string, int> lMacAddress_testing)
 			nValue++;
 		}
 	}
-	std::cout << "[errorvalue:nvalue] "<< error_value << ":" << nValue << std::endl;
+	cout << "[errorvalue:nvalue] "<< error_value << ":" << nValue << endl;
 	error_value=error_value / nValue;
 	return error_value;
 }
--- Prove_WLAN/Prove_WLAN/MAC_test.h
+++ Prove_WLAN/Prove_WLAN/MAC_test.h
@@ -2,14 +2,15 @@
 #include <map>
 #include <string>
 #include <iostream>
+using namespace std;
 class MAC_test
 {
 public:
 	MAC_test(void);
 	~MAC_test(void);
-	void InsertElement(std::string, int iRSSI);
-	float ErrorValue(std::map<std::string, int> lMacAddress_testing);
+	void InsertElement(string, int iRSSI);
+	float ErrorValue(map<string, int> lMacAddress_testing);
 private:
-	std::map<std::string, int> lMacAddress;
+	map<string, int> lMacAddress;
 };
 
