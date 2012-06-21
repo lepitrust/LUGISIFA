feca7d5f3a2636f4aa22dc59110e433a1a0930f0
Luca <l.capano87@gmail.com>
2012-06-21 16:21:28 +0200

    Prima bozza di servizio
    Ho fatto una prima struttura di un servizio generico windows che si
    occupa di gestire anche un log eventi: ora scrive solo se è in fase di
    start e di stop...
    I log si possono facilmente osservare tramite il visualizzatore eventi
    di windows, mentre il servizio è visibile tra i servizi solo dopo
    l'installazione.
    Per installare il servizio consiglio questa pagina:
    http://www.pierotofy.it/pages/guide_tutorials/Csharp/Creare_un_servizio/
    Comunque ho testato e funziona perfettamente. Ora non rimane che
    agganciarci la parte che sfrutta le WLAN e siamo a cavallo ;)
    PS: Gian è già al corrente di tutto visto che è lo stesso tipo di
    progetto che abbiamo tirato su assieme una mattina ;)

---
+++ Prova_Servizio/Prova_Servizio/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Prova_Servizio
+{
+    public partial class Service_PROVA : ServiceBase
+    {
+        public Service_PROVA()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+
+        //NB: é possibile ridefinire il comportamento OnContinue, OnPause e OnShutDown allo stesso modo
+    }
+}
